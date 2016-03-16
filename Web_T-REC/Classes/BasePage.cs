35088a43ddd63a307bdc4d75b91cbc6e02fbd069
hmooza <hmoopatongko@gmail.com>
2016-03-17 00:41:53 +0700

    add base class

---
+++ Web_T-REC/Classes/BasePage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_T_REC
+{
+    public class BasePage : System.Web.UI.Page
+    {
+        protected Web_T_REC.DataModel.Entities context;
+
+        protected override void OnInit(EventArgs e)
+        {
+            // DB Context
+            context = NewEntities();
+            
+            base.OnInit(e);
+        }
+
+        protected Web_T_REC.DataModel.Entities NewEntities()
+        {
+            var dbContext = new Web_T_REC.DataModel.Entities();
+            // Default Connection String
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+
+            //dbContext.Database.Connection.ConnectionString = this.ConnectionString;
+            dbContext.Database.Connection.ConnectionString = connectionString;
+            dbContext.Configuration.LazyLoadingEnabled = true;
+            return dbContext;
+        }
+    }
+}
